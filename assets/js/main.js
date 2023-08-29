cbfefb725f004f33284805389b18fdbf6c0f0cee
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2023-08-29 19:55:51 -0300

    Introdução à aba de detalhes dos Pokemons

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,7 @@ let offset = 0
 function loadPokemonItems(offset, limit) {
   pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
     const newHtml = pokemons.map((pokemon) => `
+    <a class="openPokemonDetailPage" href="pokemonDetail.html" target="_blanket">
       <li class="pokemon ${pokemon.type}">
       <span class="number">#${pokemon.number}</span>
       <span class="name">${pokemon.name}</span>
@@ -18,6 +19,7 @@ function loadPokemonItems(offset, limit) {
           alt="${pokemon.name}">
       </div>
     </li>
+</a>
   `).join('')
     pokemonList.innerHTML += newHtml
   })
---
+++ assets/js/pokemonDetail.js
@@ -0,0 +1,4 @@
+const openTabDetail = document.getElementById('openPokemonDetail')
+// como eu posso pegar todas as informações dos pokemons
+// https://pokeapi.co/api/v2/pokemon/ivysaur?offset=0&limit=10
+
