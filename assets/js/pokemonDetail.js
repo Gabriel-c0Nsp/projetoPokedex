0c11ad50a46d4b2545416a92518f7ecdcb5406ef
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2023-08-31 21:16:32 -0300

    requisição dinâmica de altura e largura do pokemon

--- assets/js/pokemonDetail.js
+++ assets/js/pokemonDetail.js
@@ -6,12 +6,13 @@ fetch(urlPokemonDetail)
 })
 .then((data) => {
   console.log(data)
-  document.getElementById('pokemonName').innerHTML = data['name']
+  document.getElementById('pokemonName').innerHTML = data['name'] //Pokemon name
   let idDetail = '#' + data['id']
-
-  document.getElementById('pokemonNumber').innerHTML = idDetail 
-  let img = data['sprites']['other']['dream_world']['front_default']
-  document.getElementById('pokemonDetailImage').setAttribute('src', img)
+  document.getElementById('pokemonNumber').innerHTML = idDetail // Pokemon id(number)
+  let img = data['sprites']['other']['dream_world']['front_default'] 
+  document.getElementById('pokemonDetailImage').setAttribute('src', img) // Pokemon's image
+  document.getElementById('infoHeight').innerHTML = data['height'] // Pokemon height
+  document.getElementById('infoWeight').innerHTML = data['weight'] // Pokemon height
 })
 .catch((erro => {
   console.log('erro' + erro)
