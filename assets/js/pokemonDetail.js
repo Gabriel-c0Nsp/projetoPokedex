84661420a3c19f6ca17277eda444c99177abbeb7
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2023-09-02 04:13:35 -0300

    Formatação

--- assets/js/pokemonDetail.js
+++ assets/js/pokemonDetail.js
@@ -1,51 +1,51 @@
- const urlParams = new URLSearchParams (window.location.search)
+const urlParams = new URLSearchParams(window.location.search)
 const pokemonName = urlParams.get('name')
 
 const urlPokemonDetail = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
 
 
 fetch(urlPokemonDetail)
-.then((response) => {
-  return response.json();
-})
-.then((data) => {
-  console.log(data)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data)
 
-  document.getElementById('pokemonName').innerHTML = data['name'] //Pokemon name
-  let idDetail = '#' + data['id']
+    document.getElementById('pokemonName').innerHTML = data['name'] //Pokemon name
+    let idDetail = '#' + data['id']
 
-  document.getElementById('pokemonNumber').innerHTML = idDetail // Pokemon id(number)
+    document.getElementById('pokemonNumber').innerHTML = idDetail // Pokemon id(number)
 
-  let img = data['sprites']['other']['dream_world']['front_default'] 
-  document.getElementById('pokemonDetailImage').setAttribute('src', img) // Pokemon's image
+    let img = data['sprites']['other']['dream_world']['front_default']
+    document.getElementById('pokemonDetailImage').setAttribute('src', img) // Pokemon's image
 
-  if (data["height"] / 10 < 1) {
-  let heightDetail = data["height"] / 10 + ' cm'
-  document.getElementById('infoHeight').innerHTML = heightDetail // Pokemon height
-  }
-  else if (data["height"] / 10 >= 1) {
-  let heightDetail = data["height"] / 10 + ' m'
-  document.getElementById('infoHeight').innerHTML = heightDetail // Pokemon height
-  }
+    if (data["height"] / 10 < 1) {
+      let heightDetail = data["height"] / 10 + ' cm'
+      document.getElementById('infoHeight').innerHTML = heightDetail // Pokemon height
+    }
+    else if (data["height"] / 10 >= 1) {
+      let heightDetail = data["height"] / 10 + ' m'
+      document.getElementById('infoHeight').innerHTML = heightDetail // Pokemon height
+    }
 
-  let weightDetail = data["weight"] / 10 + ' kg'
-  document.getElementById('infoWeight').innerHTML = weightDetail // Pokemon height
+    let weightDetail = data["weight"] / 10 + ' kg'
+    document.getElementById('infoWeight').innerHTML = weightDetail // Pokemon height
 
-  document.getElementById('infoAbilities').innerHTML = data['abilities']
+    document.getElementById('infoAbilities').innerHTML = data['abilities']
 
 
 
-  let abilitiesDetail = document.getElementById('infoAbilities');
-  abilitiesDetail.innerHTML = ''
+    let abilitiesDetail = document.getElementById('infoAbilities');
+    abilitiesDetail.innerHTML = ''
 
-  data['abilities'].forEach((abilityData) => {
-  let abilityName = abilityData['ability']['name'];
-  let abilityElement = document.createElement('li');
-  abilityElement.textContent = abilityName;
-  abilitiesDetail.appendChild(abilityElement);
+    data['abilities'].forEach((abilityData) => {
+      let abilityName = abilityData['ability']['name'];
+      let abilityElement = document.createElement('li');
+      abilityElement.textContent = abilityName;
+      abilitiesDetail.appendChild(abilityElement);
     })
 
-})
-.catch((erro => {
-  console.log('erro' + erro)
-}))
+  })
+  .catch((erro => {
+    console.log('erro' + erro)
+  }))
