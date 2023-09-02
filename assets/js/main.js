59eaf6f1955cfecbb3c25dca8766540821452182
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2023-09-02 04:09:03 -0300

    Alguns aprimoramentos da pokedex

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ let offset = 0
 function loadPokemonItems(offset, limit) {
   pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
     const newHtml = pokemons.map((pokemon) => `
-    <a class="openPokemonDetailPage" href="pokemonDetail.html" target="_blanket">
+    <a class="openPokemonDetailPage" href="pokemonDetail.html?name=${pokemon.name}" target="_blanket">
       <li class="pokemon ${pokemon.type}">
       <span class="number">#${pokemon.number}</span>
       <span class="name">${pokemon.name}</span>
--- assets/js/pokemonDetail.js
+++ assets/js/pokemonDetail.js
@@ -1,4 +1,8 @@
-let urlPokemonDetail = "https://pokeapi.co/api/v2/pokemon/bulbasaur"
+ const urlParams = new URLSearchParams (window.location.search)
+const pokemonName = urlParams.get('name')
+
+const urlPokemonDetail = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+
 
 fetch(urlPokemonDetail)
 .then((response) => {
@@ -6,13 +10,41 @@ fetch(urlPokemonDetail)
 })
 .then((data) => {
   console.log(data)
+
   document.getElementById('pokemonName').innerHTML = data['name'] //Pokemon name
   let idDetail = '#' + data['id']
+
   document.getElementById('pokemonNumber').innerHTML = idDetail // Pokemon id(number)
+
   let img = data['sprites']['other']['dream_world']['front_default'] 
   document.getElementById('pokemonDetailImage').setAttribute('src', img) // Pokemon's image
-  document.getElementById('infoHeight').innerHTML = data['height'] // Pokemon height
-  document.getElementById('infoWeight').innerHTML = data['weight'] // Pokemon height
+
+  if (data["height"] / 10 < 1) {
+  let heightDetail = data["height"] / 10 + ' cm'
+  document.getElementById('infoHeight').innerHTML = heightDetail // Pokemon height
+  }
+  else if (data["height"] / 10 >= 1) {
+  let heightDetail = data["height"] / 10 + ' m'
+  document.getElementById('infoHeight').innerHTML = heightDetail // Pokemon height
+  }
+
+  let weightDetail = data["weight"] / 10 + ' kg'
+  document.getElementById('infoWeight').innerHTML = weightDetail // Pokemon height
+
+  document.getElementById('infoAbilities').innerHTML = data['abilities']
+
+
+
+  let abilitiesDetail = document.getElementById('infoAbilities');
+  abilitiesDetail.innerHTML = ''
+
+  data['abilities'].forEach((abilityData) => {
+  let abilityName = abilityData['ability']['name'];
+  let abilityElement = document.createElement('li');
+  abilityElement.textContent = abilityName;
+  abilitiesDetail.appendChild(abilityElement);
+    })
+
 })
 .catch((erro => {
   console.log('erro' + erro)
